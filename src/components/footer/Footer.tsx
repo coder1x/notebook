0dfdcd4fbd5ddffafd4080b209d78353a7533e8e
Kliushin Anton <c.o.d.e.r.1@mail.ru>
2023-03-13 21:43:13 +0500

    подправил адаптив

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import Props from './footerType';
 
@@ -9,30 +9,41 @@ const Footer: FC<Props> = ({
   totalCompleted = 0,
   type = 'project',
 }) => {
-  return (
-    <ul className="footer">
-      <li className="footer__item">
-        <span className="footer__text-total">Всего:</span>
-        <span className="footer__number-total">{total}</span>
-      </li>
+  const [isActive, setIsActive] = useState(false);
 
-      {type !== 'project' && (
-        <>
-          <li className="footer__item">
-            <span className="footer__text-total">Задач:</span>
-            <span className="footer__number-total">{totalTasks}</span>
-          </li>
-          <li className="footer__item">
-            <span className="footer__text-total">Выполняемых:</span>
-            <span className="footer__number-total">{totalPerformed}</span>
-          </li>
-          <li className="footer__item">
-            <span className="footer__text-total">Завершённых:</span>
-            <span className="footer__number-total">{totalCompleted}</span>
-          </li>
-        </>
-      )}
-    </ul>
+  const handleButtonClick = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className="footer">
+      <button className="footer__informer" onClick={handleButtonClick}>
+        Ⓘ
+      </button>
+      <ul className={`footer__list ${isActive ? ' footer__list_visible' : ''}`}>
+        <li className="footer__item">
+          <span className="footer__text-total">Всего:</span>
+          <span className="footer__number-total">{total}</span>
+        </li>
+
+        {type !== 'project' && (
+          <>
+            <li className="footer__item">
+              <span className="footer__text-total">Задач:</span>
+              <span className="footer__number-total">{totalTasks}</span>
+            </li>
+            <li className="footer__item">
+              <span className="footer__text-total">Выполняемых:</span>
+              <span className="footer__number-total">{totalPerformed}</span>
+            </li>
+            <li className="footer__item">
+              <span className="footer__text-total">Завершённых:</span>
+              <span className="footer__number-total">{totalCompleted}</span>
+            </li>
+          </>
+        )}
+      </ul>
+    </div>
   );
 };
 
